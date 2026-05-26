95689d0a790003eac6f19f5d25c15ca4eb9e183c
telmo <fieuxtelmo@gmail.com>
2026-05-26 20:35:11 +0200

    modif de la fpu et modif des tests

--- sw/app/test_cmov/test_fcmov.s
+++ sw/app/test_cmov/test_fcmov.s
@@ -22,37 +22,36 @@ actual_code:
     la   t2, buffer
 
     li   t0, 0x40400000      # 3.0f
-    sw   t0, 0(t2)           # ft0 sera à buffer + 0
+    sw   t0, 0(t2)
 
     li   t0, 0x00000000      # 0.0f
-    sw   t0, 8(t2)           # ft1 sera à buffer + 8 (Nouvelle frontière 64-bit)
+    sw   t0, 8(t2)
 
     li   t1, 0x41000000      # 8.0f
-    sw   t1, 16(t2)          # ft2 sera à buffer + 16 (Nouvelle frontière 64-bit)
+    sw   t1, 16(t2)
 
-    # Le fence force le Write Buffer du HPDCACHE à tout valider d'un coup
     fence
-    fence.i                  # Optionnel mais recommandé : synchronise les pipelines
+    fence.i
 
-    # === Chargement dans les registres FPU ===
-    flw  ft0, 0(t2)          # Reçoit 3.0f
-    flw  ft1, 8(t2)          # Reçoit 0.0f
-    flw  ft2, 16(t2)         # Reçoit 8.0f (Sera valide cette fois-ci !)
+    # chargement dans les registres FPU
+    flw  ft0, 0(t2)
+    flw  ft1, 8(t2)
+    flw  ft2, 16(t2)
     nop
     nop
 
-    # === 4. Test FCMOV normal ===
+    # test comportement normal
     .insn r4 0x0b, 1, 0, ft3, ft0, ft1, ft2
 
-    # === 5. Test forwarding rs3 ===
+    # test forwarding rs3
     li   t0, 0x3F800000      # 1.0f
     sw   t0, 0(t2)
     nop
-    flw  ft1, 0(t2)          # ft1 = 1.0
+    flw  ft1, 0(t2)
     fadd.s ft2, ft2, ft3
     .insn r4 0x0b, 1, 0, ft3, ft0, ft1, ft2
 
-    # === 6. Test stall rs3 ===
+    # test stall rs3
     li   t0, 0x40A00000      # 5.0f
     sw   t0, 0(t2)
     nop
@@ -62,9 +61,6 @@ actual_code:
 end:
     j end
 
-# =========================================================
-# VRAIE SECTION DE DONNÉES ACCESSIBLE EN LECTURE/ÉCRITURE
-# =========================================================
 .section .data
 .align 4
 buffer:
